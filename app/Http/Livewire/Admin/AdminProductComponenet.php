1b1f30f0eadd49f7f3cde94f89a4e9cceff42ff3
kamrul <kamrul1728@cseku.ac.bd>
2021-06-22 23:47:39 +0600

    Admin product page added

--- app/Http/Livewire/Admin/AdminProductComponenet.php
+++ app/Http/Livewire/Admin/AdminProductComponenet.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\Product;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class AdminProductComponenet extends Component
 {
+    use WithPagination;
     public function render()
     {
-        return view('livewire.admin.admin-product-componenet');
+        $products=Product::paginate(5);
+        return view('livewire.admin.admin-product-componenet',['products'=>$products])->layout("layouts.base");
     }
 }
